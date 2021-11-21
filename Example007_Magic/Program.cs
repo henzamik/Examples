2d72d6eb71d71354d6dddfc96b7a51630b1da9e4
Kseniia <henzamik@gmail.com>
2021-11-22 00:57:33 +0300

    Add Example007_Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 40, yc = 30,
+
+
+Console.SetCursorPosition(xa,ya);
+Console.Writlinane("+");
+Console.SetCursorPosition(xb,yb);
+Console.Writlinane("+");
+Console.SetCursorPosition(xc,yc);
+Console.Writlinane("+");
